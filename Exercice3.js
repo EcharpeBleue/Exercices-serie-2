935debe992ef08ab91c9bed1a66dd41d07f77935
EcharpeBleue <zawiczrczarny@gmail.com>
2023-10-16 15:49:28 +0200

    Exercice n°4

---
+++ Exercice3.js
@@ -0,0 +1,4 @@
+let longueur = prompt(`Quelle est la longueur du rectangle ?`)
+let largeur = prompt(`Quelle est la largeur du rectangle ?`)
+let surface = longueur*largeur;
+console.log(`le rectangle de longueur `+longueur+` cm et de largeur `+largeur+` cm a pour surface `+surface+` cm2`)

69cd2ec5883ec85f1352d5023b1a66ad3ec4ffb2
EcharpeBleue <zawiczrczarny@gmail.com>
2023-10-20 10:31:30 +0200

    Exercice 5
    Utilisation de defer

--- Exercice5.js
+++ Exercice5.js
@@ -1,11 +1,11 @@
 let x = prompt(`Veuillez saisir un premier nombre.`)
 let y = prompt(`Veuillez saisir un deuxième nombre.`)
-const paragraphe = "paragraphe"
-if (x>y) {
-    let paragraphe = document.getElementById("p1"); // récupérer l'élement HTML
+let paragraphe = document.getElementById("p1"); // récupérer l'élement HTML
+if (x>y) 
+{
     paragraphe.innerHTML="Le premier numéro est plus grand que le second";// modifier la propriété innerHTML
 }
-else {
-    let paragraphe = document.getElementById("p1"); // récupérer l'élement HTML
+else 
+{
     paragraphe.innerHTML="Le deuxième numéro est plus grand que le premier";// modifier la propriété innerHTML
 }
